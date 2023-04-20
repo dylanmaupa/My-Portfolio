370c7f25c803e5bd04cd228df031cc44005812c1
dylanmaupa <114060288+dylanmaupa@users.noreply.github.com>
2023-04-20 14:59:05 +0200

    #

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// const scrollreveal = require("scrollreveal");
+
 const hire = document.getElementById("hire");
 const hireMe = document.getElementById("hire-me");
 const cancel = document.getElementById("cancel");
@@ -6,7 +8,24 @@ hire.onclick = function () {
   hireMe.style.right = 0;
 
   cancel.onclick = function () {
-    hireMe.style.right = -150 + "%";
+    hireMe.style.right = -200 + "%";
   };
 };
 
+var slideUp = {
+    distance: '400%',
+    origin: 'left',
+    opacity: null
+};
+
+window.matchMedia("")
+
+ScrollReveal().reveal("#my-skills, #projects", slideUp, {reset:true});
+
+ScrollReveal().reveal("#contact",{
+    rotate: {
+        x: 20,
+        z: 20
+    },
+    delay: 200, easing: "ease-out"
+});
